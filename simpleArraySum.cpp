706fab9c2f693d80fa37f0aa4e45aa5aeb3669a1
Habib Abdurrasyid <43912088+kawaii801@users.noreply.github.com>
2018-10-06 23:41:30 +0700

    Add files via upload

---
+++ simpleArraySum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int simpleArraySum (int n){
+    int sum =0, i=0;
+    int a[n];
+    while (n>0){
+    	cin >> a[i];
+        sum = sum + a[i];
+        n--;i++;
+    }
+    return sum;
+}
+int main (){
+    int n;
+    cin >> n;
+cout << simpleArraySum(n);
+
+}
